f706c4030fbe92fcc780b30a76b42ad042b51376
Biblioklept <61851290+Biblioklept@users.noreply.github.com>
2023-05-28 12:27:02 -0400

    Scripts page, MO2 scripts, and main page update.

---
+++ scripts/dl/mo2/install-nodel.bat
@@ -0,0 +1,27 @@
+@echo off
+mkdir MO2
+cd MO2
+curl https://www.7-zip.org/a/7zr.exe -L -o 7zr.exe
+curl https://github.com/ModOrganizer2/modorganizer/releases/download/v2.4.4/Mod.Organizer-2.4.4.7z -L -o mo2.7z
+7zr x mo2.7z
+cd plugins
+curl https://github.com/Kezyma/ModOrganizer-Plugins/releases/download/rootbuilder/rootbuilder.4.4.2.zip -L -o rb.zip
+curl https://www.7-zip.org/a/7zr.exe -L -o 7zr.exe
+curl https://www.7-zip.org/a/7z2201-extra.7z -L -o 7za.7z
+7zr x 7za.7z
+7za x -y rb.zip
+del rb.zip
+del 7zr.exe
+del 7za.7z
+del 7za.dll
+del 7za.exe
+del 7zxa.dll
+del history.txt
+del Licence.txt
+del readme.txt
+rd /S /Q Far
+rd /S /Q x64
+cd ..
+del mo2.7z
+del 7zr.exe
+exit
---
+++ scripts/index.md
@@ -0,0 +1,15 @@
+# Biblioklept's Site - [HOME](https://biblioklept.github.io/)
+My Scripts
+
+### Mod Organizer 2 Scripts
+
+- [Install Script](./dl/mo2/install.bat)
+> Installs Mod Organizer 2 and the RootBuilder plugin by Kezyma, then cleans up the folder.
+- [NoDel Install Script](./dl/mo2/install-nodel.bat)
+> Same as above, without the deletion.
+- [NoRB Install Script](./dl/mo2/install-norb.bat)
+> Same as the install script, without the RootBuilder plugin.
+- [NoRB NoDel Install Script](./dl/mo2/install-norb-nodel.bat)
+> Same as above, without the deletion.
+- [AIO File](./dl/mo2/aio-mo2.7z)
+> All the scripts in one 7-zip file.
